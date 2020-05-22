db63d6f711baf2f507cfdfe797bcc7ea8a58d24c
sobralmps <sobralmps@gmail.com>
2020-05-22 02:53:34 -0300

    Web scraping com beautiful soap

--- CoronaApp/views.py
+++ CoronaApp/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+import requests
+from bs4 import BeautifulSoup
 
-
-def mensagem(request):
-    mensagem = "Hello World"
-    return HttpResponse(mensagem)
 
 def home(request):
-    return render(request, 'CoronaApp/Piloto.html')
+    data = {}
+
+    url = "https://www.worldometers.info/coronavirus/country/brazil/"
+    html = requests.get(url)
+
+    bs = BeautifulSoup(html.content, 'html.parser')
+
+    casos_totais = bs.find_all('div', class_='maincounter-number')[0].get_text()
+    mortes_totais = bs.find_all('div', class_='maincounter-number')[1].get_text()
+    recuperados = bs.find_all('div', class_='maincounter-number')[2].get_text()
+
+    casos_ativos = bs.find_all('div', class_='number-table-main')[0].get_text()
+    casos_leves = bs.find_all('span', class_='number-table')[0].get_text()
+    casos_graves = bs.find_all('span', class_='number-table')[1].get_text()
+
+    data['casostotais'] = casos_totais
+    data['mortestotais'] = mortes_totais
+    data['recuperados'] = recuperados
+
+    return render(request, 'CoronaApp/Piloto.html', data)
