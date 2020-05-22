8efc890cd459b49cab588141bee69aa68f0e2f56
inaba2 <guilherme.inaba2@gmail.com>
2020-05-22 14:11:35 -0300

    tentativa do grafico

--- CoronaApp/views.py
+++ CoronaApp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 import requests
 from bs4 import BeautifulSoup
-
+import matplotlib.pyplot as plt
 
 def home(request):
     info = {}
@@ -31,5 +31,13 @@ def home(request):
     info['casosativos'] = casos_ativos
     info['casosleves'] = casos_leves
     info['casosgraves'] = casos_graves
+
+    y_pos = list(range(len(casos_totais)))
+    plt.bar(y_pos, taxa_desemprego[::-1], align='center', alpha=0.5)
+    plt.xticks(y_pos, casos_totais[::-1], rotation=70)
+    plt.ylabel('Total cases')
+    plt.title('Persons affected by Corona virus')
+    plt.savefig('Corona-analysis.png', dpi=600)
+    plt.show()
 
     return render(request, 'CoronaApp/Piloto.html', info)
